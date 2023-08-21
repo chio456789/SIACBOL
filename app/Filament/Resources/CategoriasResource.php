a2a0b47fb4c4f8ca9866eedefaed580a003d7adf
chio456789 <roscee777@gmail.com>
2023-08-21 02:53:29 -0400

    uso de filament dashboard

---
+++ app/Filament/Resources/CategoriasResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoriasResource\Pages;
+use App\Filament\Resources\CategoriasResource\RelationManagers;
+use App\Models\Categorias;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoriasResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Forms\Components\TextInput::make('nombre'),
+            Forms\Components\TextInput::make('descripcion'),
+       
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre'),
+                Tables\Columns\TextColumn::make('descripcion'),
+             
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategorias::route('/'),
+            'create' => Pages\CreateCategorias::route('/create'),
+            'edit' => Pages\EditCategorias::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ app/Filament/Resources/ServiciosResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ServiciosResource\Pages;
+use App\Filament\Resources\ServiciosResource\RelationManagers;
+use App\Models\Service;
+use App\Models\Servicios;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ServiciosResource extends Resource
+{
+    protected static ?string $model = Service::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nombre'),
+                Forms\Components\TextInput::make('descripcion'),
+                Forms\Components\TextInput::make('precio'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nombre'),
+                Tables\Columns\TextColumn::make('descripcion'),
+                Tables\Columns\TextColumn::make('precio'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListServicios::route('/'),
+            'create' => Pages\CreateServicios::route('/create'),
+            'edit' => Pages\EditServicios::route('/{record}/edit'),
+        ];
+    }    
+}
